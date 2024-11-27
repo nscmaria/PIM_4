cbadcf774afe00d4a91bee67451364ff613ddc23
nscma <mariards880@gmail.com>
2024-11-26 23:15:10 -0300

    commit

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -2,27 +2,24 @@ package com.example.application
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    lateinit var btn_login: Button
+
+    override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_telalogin)
+
+        btn_login = findViewById(R.id.btn_login)
+
+        btn_login.setOnClickListener {
+
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
 
         }
-    }
-    private fun activityMain2() {
-        val activityMain2 = Intent(this, activityMain2()::class.java)
-        startActivity(activityMain2)
-
-
     }
 }
