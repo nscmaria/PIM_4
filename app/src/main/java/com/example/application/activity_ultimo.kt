7074aedf09f112f9bc61842f9916f9b419ebc606
nscma <mariards880@gmail.com>
2024-11-27 22:42:08 -0300

    finalizado as telas, faltando ligar botoes

--- app/src/main/java/com/example/application/activity_ultimo.kt
+++ app/src/main/java/com/example/application/activity_ultimo.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class activity_ultimo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_ultimo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
